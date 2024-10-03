6f669298d16a514612462200b9fb392bc55c6be5
BenjaminP3000 <benjaminpersson@hotmail.se>
2024-10-03 14:56:27 +0200

    Add project files.

---
+++ MonsterIsh/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MonsterIsh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
